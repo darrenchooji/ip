1388a21b355c3e1fde68b7d05a1f5fc90ca671ba
darrenchooji <darrenchoojuninn@gmail.com>
2025-01-23 22:23:17 +0800

    Level-0

---
+++ src/main/java/Fiona.java
@@ -0,0 +1,11 @@
+public class Fiona {
+    public static void main(String[] args) {
+        String line = "------------------------------------------";
+        System.out.println(line);
+        System.out.println("Hello! I'm Fiona.");
+        System.out.println("What can I do for you?");
+        System.out.println(line);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line);
+    }
+}
