720ff3abaacd2e79ac9e69e8913e592d49a730ed
darrenchooji <darrenchoojuninn@gmail.com>
2025-01-23 22:49:16 +0800

    Level-0 done

--- src/main/java/Fiona.java
+++ src/main/java/Fiona.java
@@ -1,19 +1,7 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
 public class Fiona {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+    public static void main(String[] args) {
         String line = "------------------------------------------";
         System.out.println(line+"\nHello! I'm Fiona.\nWhat can I do for you?\n"+line);
-        while (true) {
-            String input = br.readLine();
-            System.out.println(line);
-            if (input.equals("bye")) break;
-            System.out.println(input+"\n"+line);
-        }
         System.out.println("Bye. Hope to see you again soon!\n"+line);
     }
 }
