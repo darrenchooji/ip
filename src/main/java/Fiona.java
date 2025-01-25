7c88b35208884610c5b712b2d615675e9900a4b0
darrenchooji <darrenchoojuninn@gmail.com>
2025-01-25 09:44:05 +0800

    Level-5 done

--- src/main/java/Fiona.java
+++ src/main/java/Fiona.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class Fiona {
     private static String line = "-------------------------------------------------------------";
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, FionaException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         List<Task> taskList = new ArrayList<>();
@@ -19,55 +19,80 @@ public class Fiona {
             if (action.equals("bye")) break;
 
             System.out.println(line);
-            if (action.equals("todo")) {
-                String name = inputs[1].trim();
-                Task t = new Todo(name);
-                taskList.add(t);
-                System.out.println("Got it. I've added this task:");
-                System.out.println(t);
-                System.out.println("Now you have "+taskList.size()+" task(s) in the list.");
-
-            } else if (action.equals("deadline")) {
-                String[] parts = inputs[1].split("/by", 2);
-                String name = parts[0].trim();
-                String deadline = parts[1].trim();
-                Task t = new Deadline(name, deadline);
-                taskList.add(t);
-                System.out.println("Got it. I've added this task:");
-                System.out.println(t);
-                System.out.println("Now you have "+taskList.size()+" task(s) in the list.");
-
-            } else if (action.equals("event")) {
-                String[] fromSplit = inputs[1].split("/from", 2);
-                String name = fromSplit[0].trim();
-                
-                String[] toSplit = fromSplit[1].split("/to", 2);
-                String from = toSplit[0].trim(); 
-                String to = toSplit[1].trim(); 
-                Task t = new Event(name, from, to);
-                taskList.add(t);
-                System.out.println("Got it. I've added this task:");
-                System.out.println(t);
-                System.out.println("Now you have "+taskList.size()+" task(s) in the list.");
-
-            } else if (action.equals("list")) {
-                for (Task t : taskList) {
-                    System.out.println(t.getId() + ". "+t);
+            try {
+                if (action.equals("todo")) {
+                    if (inputs.length < 2 || inputs[1].trim().isEmpty()) {
+                        throw new FionaException("The description of a todo cannot be empty.");
+                    }
+                    String name = inputs[1].trim();
+                    Task t = new Todo(name);
+                    taskList.add(t);
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println(t);
+                    System.out.println("Now you have "+taskList.size()+" task(s) in the list.");
+    
+                } else if (action.equals("deadline")) {
+                    if (inputs.length < 2 || inputs[1].trim().isEmpty() || !inputs[1].contains("/by")) {
+                        throw new FionaException("The description of a deadline must include a '/by' clause.");
+                    }
+                    String[] parts = inputs[1].split("/by", 2);
+                    String name = parts[0].trim();
+                    String deadline = parts[1].trim();
+                    Task t = new Deadline(name, deadline);
+                    taskList.add(t);
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println(t);
+                    System.out.println("Now you have "+taskList.size()+" task(s) in the list.");
+    
+                } else if (action.equals("event")) {
+                    if (inputs.length < 2 || inputs[1].trim().isEmpty() || !inputs[1].contains("/from") || !inputs[1].contains("/to")) {
+                        throw new FionaException("The description of an event must include '/from' and '/to' clauses.");
+                    }
+                    String[] fromSplit = inputs[1].split("/from", 2);
+                    String name = fromSplit[0].trim();
+                    
+                    String[] toSplit = fromSplit[1].split("/to", 2);
+                    String from = toSplit[0].trim(); 
+                    String to = toSplit[1].trim(); 
+                    Task t = new Event(name, from, to);
+                    taskList.add(t);
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println(t);
+                    System.out.println("Now you have "+taskList.size()+" task(s) in the list.");
+    
+                } else if (action.equals("list")) {
+                    for (Task t : taskList) {
+                        System.out.println(t.getId() + ". "+t);
+                    }
+    
+                } else if (action.equals("mark")) {
+                    if (inputs.length < 2) {
+                        throw new FionaException("You must specify a valid task number to mark as done.");
+                    }
+                    int id = Integer.parseInt(inputs[1])-1;
+                    taskList.get(id).setDone();
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println(taskList.get(id));
+    
+                } else if (action.equals("unmark")) {
+                    if (inputs.length < 2) {
+                        throw new FionaException("You must specify a valid task number to mark as not done yet.");
+                    }
+                    int id = Integer.parseInt(inputs[1])-1;
+                    taskList.get(id).setUndone();
+                    System.out.println("OK, I've marked this task as not done yet :");
+                    System.out.println(taskList.get(id));
+    
+                } else {
+                    throw new FionaException("I'm sorry, but I don't know what that means :-(");
                 }
-
-            } else if (action.equals("mark")) {
-                int id = Integer.parseInt(inputs[1])-1;
-                taskList.get(id).setDone();
-                System.out.println("Nice! I've marked this task as done:");
-                System.out.println(taskList.get(id));
-
-            } else {
-                int id = Integer.parseInt(inputs[1])-1;
-                taskList.get(id).setUndone();
-                System.out.println("OK, I've marked this task as not done yet :");
-                System.out.println(taskList.get(id));
-
-            }
+            } catch (FionaException fE) {
+                System.out.println(fE.getMessage());
+            } catch (NumberFormatException e) {
+                System.out.println("The task number you specified must be a valid integer!");
+            } catch (IndexOutOfBoundsException e) {
+                System.out.println("The task number you specified does not exist!");
+            } 
             System.out.println(line);
         }
         System.out.println(line+"\nBye. Hope to see you again soon!\n"+line);
---
+++ src/main/java/FionaException.java
@@ -0,0 +1,5 @@
+public class FionaException extends Exception {
+    public FionaException(String message) {
+        super(message);
+    }
+}
