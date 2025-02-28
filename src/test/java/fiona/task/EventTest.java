3244240ca070387917fab0c829d701f346301663
darrenchooji <darrenchoojuninn@gmail.com>
2025-02-28 11:05:29 +0800

    Update EventTest.java
    Altered testing deadline dates to a later date in 2035 to ensure that the same test commands can be reused in the future until 2035.

--- src/test/java/fiona/task/EventTest.java
+++ src/test/java/fiona/task/EventTest.java
@@ -10,7 +10,7 @@ public class EventTest {
     @Test
     void constructor_validFormat_success() {
         Assertions.assertDoesNotThrow(() -> {
-            Event event = new Event("Team meeting", "2027-02-20 1400", "2027-02-20 1500");
+            Event event = new Event("Team meeting", "2035-02-20 1400", "2035-02-20 1500");
             String eventStr = event.toString();
             Assertions.assertTrue(eventStr.contains("Team meeting"));
             Assertions.assertTrue(eventStr.contains("from:"));
@@ -21,7 +21,7 @@ public class EventTest {
     @Test
     void constructor_invalidFormat_throwsException() {
         FionaException ex = Assertions.assertThrows(FionaException.class, () -> {
-            new Event("Team meeting", "2027-02-20T1400", "2027-02-20 1500");
+            new Event("Team meeting", "2035-02-20T1400", "2035-02-20 1500");
         });
         Assertions.assertTrue(ex.getMessage().contains("Invalid date-time format for event"),
                 "Expected an error regarding the date-time format");
@@ -30,7 +30,7 @@ public class EventTest {
     @Test
     void constructor_startAfterEnd_throwsException() {
         FionaException ex = Assertions.assertThrows(FionaException.class, () -> {
-            new Event("Team meeting", "2027-02-20 1600", "2027-02-20 1500");
+            new Event("Team meeting", "2035-02-20 1600", "2035-02-20 1500");
         });
         Assertions.assertTrue(ex.getMessage().contains("Start date time cannot be after end date time"),
                 "Expected an error message indicating that the event's start time is after its end time.");
